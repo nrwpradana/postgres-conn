442c287d608bcb94f3e82dcbd0f80223ad2ada48
Nadhiar Pradana <nadhiar11@gmail.com>
2023-07-23 20:18:51 +0700

    Edit App

--- app.py
+++ app.py
@@ -1,27 +1,30 @@
 import streamlit as st
-import pandas as pd
 from connection import PostgresConnection
+import json
 
 # Assuming you have already uploaded the 'postgres_connection.py' file to the Colab environment.
 
-# Upload the SQL file
-uploaded = st.file_uploader("Upload SQL file", type=["sql"])
+# Upload the database credentials file
+uploaded = st.file_uploader("Upload PostgreSQL credentials JSON file", type=["json"])
 
 if uploaded:
-    # Read the SQL file content
-    sql_content = uploaded.read().decode()
+    db_config = json.load(uploaded)
+
+    # Display the database credentials
+    st.write("Database Credentials:")
+    st.write("Database Name:", db_config.get("dbname"))
+    st.write("User:", db_config.get("user"))
+    st.write("Password:", db_config.get("password"))
+    st.write("Host:", db_config.get("host"))
+    st.write("Port:", db_config.get("port"))
 
     # Create the connection
-    conn = st.experimental_connection("postgres", type=PostgresConnection)
+    conn = st.experimental_connection("postgres", type=PostgresConnection, **db_config)
 
-    # Execute the SQL query to fetch the top 10 records
-    query = sql_content + " LIMIT 10;"
+    # Test query to verify the connection
+    query = "SELECT version();"
     result = conn.query(query)
 
     # Display the query result
-    if result is not None:
-        df = pd.DataFrame(result, columns=result[0].keys())
-        st.write("Top 10 records:")
-        st.write(df)
-    else:
-        st.write("No data retrieved from the SQL file.")
+    st.write("PostgreSQL version:")
+    st.write(result)
